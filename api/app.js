6d05be6bf9403e9197c8e438949ebc820f63589f
syazah <thesyazah@gmail.com>
2024-05-25 14:53:06 +0530

    some changes

--- api/app.js
+++ api/app.js
@@ -28,7 +28,7 @@ app.use("/api/v1", panelRouter);
 
 app.use(express.static(path.join(__dirname, "/client/dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
 });
 //ERROR HANDLE
 app.use((err, req, res, next) => {
